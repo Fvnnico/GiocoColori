6f9789efeb9415b02ba3faaa610ac60cd4ab8108
EeehGiji <107210061+Fvnnico@users.noreply.github.com>
2024-06-10 18:19:09 +0200

    Update leaderboard

--- src/script.js
+++ src/script.js
@@ -1,22 +1,32 @@
-let mainColor; // Variabile per memorizzare il colore principale
+let colorePrincipale; // Variabile per memorizzare il colore principale
 
-function getRandomRGB() {
+/* vittorie reset a 0 */
+if (localStorage.getItem("vittorie") === null) {
+    localStorage.setItem("vittorie", 0);
+}
+
+/* vittorie reset a 0 */
+if (localStorage.getItem("sconfitte") === null) {
+    localStorage.setItem("sconfitte", 0);
+}
+
+// per prendere un colore a caso
+function randomizzatoRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-function displayRandomColor() {
+function mostraColoreCasuale() {
     const element = document.getElementById("randomColorText");
-    mainColor = getRandomRGB(); // Memorizza il colore generato
-    element.textContent = `Colore RGB randomizzato: ${mainColor}`;
+    colorePrincipale = randomizzatoRGB(); // Memorizza il colore generato
+    element.textContent = `Colore RGB randomizzato: ${colorePrincipale}`;
 }
-
-displayRandomColor();
+mostraColoreCasuale();
 
 // Funzione per applicare colori randomizzati
-function applyRandomColors() {
+function applicaColoreRandomizzato() {
     const colorElements = document.querySelectorAll(".colore-random");
 
     // Scegliere casualmente un elemento da colorare con lo stesso colore di mainElement
@@ -25,33 +35,64 @@ function applyRandomColors() {
     // Assegnare colori randomizzati agli altri elementi, tranne uno
     colorElements.forEach((element, index) => {
         if (index === randomIndex) {
-            element.style.backgroundColor = mainColor; 
+            element.style.backgroundColor = colorePrincipale;
         } else {
-            element.style.backgroundColor = getRandomRGB(); 
+            element.style.backgroundColor = randomizzatoRGB();
         }
     });
 }
+applicaColoreRandomizzato();
 
-applyRandomColors();
-
-
-function checkColor(event) {
+function controllaColore(event) {
     const elementoScelto = event.target;
-
-    
-    if (elementoScelto.style.backgroundColor === mainColor) {
-        alert("Hai vinto!")
-        
+    if (elementoScelto.style.backgroundColor === colorePrincipale) {
+        alert("Hai vinto!");
+        let vittorie = parseInt(localStorage.getItem("vittorie"));
+        vittorie += 1;
+        localStorage.setItem("vittorie", vittorie.toString());
+        scoreAggiornato();
+        mostraColoreCasuale();
+        applicaColoreRandomizzato();
     } else {
-        alert("Hai perso!")
-        location.reload();
+        alert("Hai perso!");
+        let sconfitte = parseInt(localStorage.getItem("sconfitte"));
+        sconfitte += 1;
+        localStorage.setItem("sconfitte", sconfitte.toString());
+        scoreAggiornato();
+        mostraColoreCasuale();
+        applicaColoreRandomizzato();
     }
 }
 
-
+// Per ogni blocco da un colore
 const colorElements = document.querySelectorAll(".colore-random");
-colorElements.forEach(element => {
-    element.addEventListener('click', checkColor);
+colorElements.forEach((elementoColore) => {
+    elementoColore.addEventListener("click", controllaColore);
 });
 
 
+
+function scoreAggiornato() {
+    const vittorie = localStorage.getItem("vittorie");
+    const sconfitte = localStorage.getItem("sconfitte");
+    document.getElementById(
+        "vittorie"
+    ).innerText = `Numero di vittorie: ${vittorie}`;
+    document.getElementById(
+        "sconfitte"
+    ).innerText = `Numero di sconfitte: ${sconfitte}`;
+}
+scoreAggiornato();
+
+
+
+function resettaPunteggio() {
+    const vittorie = localStorage.getItem("vittorie");
+    const sconfitte = localStorage.getItem("sconfitte");
+
+    if (vittorie || sconfitte >= 1) {
+        localStorage.setItem("vittorie", 0);
+        localStorage.setItem("sconfitte", 0);
+        scoreAggiornato();
+    }
+}
